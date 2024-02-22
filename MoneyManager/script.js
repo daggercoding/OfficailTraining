91704219727c37d3a9465edddc704e98fc3a63a3
VISHAL SINGH <daggercoding@gmail.com>
2024-02-22 13:07:41 +0530

    created the update form and get the values in the form input

--- MoneyManager/script.js
+++ MoneyManager/script.js
@@ -2,22 +2,41 @@ const tableData =document.getElementById("expenseList")
 const total = document.getElementById("totalAmount")
 const spend = document.getElementById("spend")
 const submitButton =document.getElementById("submitButton")
-let Cateogory = document.getElementById("expenseCateogory")
+const Cateogory = document.getElementById("expenseCateogory")
+const remaining = document.getElementById("remaining")
+const totalCard = document.getElementById("total")
+const budgetInput =  document.getElementById("budgetInput")
+//Getting Elements to Update the Value
+// const updateCateogoryinput=document.getElementById("updateCateogory")
+// const updateOptioninput=document.getElementById("updateCateogoryOption")
+
+const updateCateogoryinput=document.getElementById("updateCateogory")
+const updatePriceinput=document.getElementById("updatePrice")
+const updateDateinput=document.getElementById("updateDate")
+//GETTING THE ELEMNTS OF UPDATE FORM 
+const updateForm=document.getElementById("updateContainer")
+
+let totalBudgetFromLocalStorage=JSON.parse(localStorage.getItem("totalBudget"))
+if(totalBudgetFromLocalStorage){
+ totalCard.innerText=totalBudgetFromLocalStorage
+}
 
 //writing the logic to add the budget decided by user
-let totalBudget = 0
-const budgetInput =  document.getElementById("budgetInput")
+let totalBudget
 budgetInput.addEventListener("change",(event)=>{
   //storing the value of user budget
   let finalBudget = event.target.value
-  totalBudget+=parseInt(finalBudget)
+  totalBudget=parseInt(finalBudget)
+
+  localStorage.setItem("totalBudget",JSON.stringify(totalBudget))
+  totalBudgetFromLocalStorage = totalBudget
   //clearing the input field
   budgetInput.value=""
   //disable the budget input form after user put his budget
   budgetInput.disabled=true
   //inserting the number to the upper div
-  const total = document.getElementById("total")
-  total.innerText=totalBudget
+  totalCard.innerText=totalBudget
+  renderList()
 })
 
 
@@ -38,7 +57,6 @@ userCateogory.addEventListener("change",(event)=>{
 
 //A blank array to store the data of input fields
 let expenseList = JSON.parse(localStorage.getItem("expenseData")) ?? []
-
 document.getElementById("expense-form").addEventListener("submit", function (e) {
    e.preventDefault();
     //here we were getting the elements by its id
@@ -74,16 +92,19 @@ document.getElementById("expense-form").addEventListener("submit", function (e) 
   // function to render the newly created data in the Table
   function renderList()
   {
-    // console.log("rendering function")
     tableData.innerText=""
-
+    //if our array length = 0
     let expenditure = 0 
-
+    if(expenseList.length==0){
+      total.innerText=0
+      spend.innerText=0
+      remaining.innerText=totalBudgetFromLocalStorage
+    }
     for(let i = 0; i<expenseList.length ; i++)
     {
     //sooting the object from the Array
      const expenses = expenseList[i]
-     expenditure+= parseInt(expenses.price)??0
+     expenditure+= parseInt(expenses.price)
 
      const newRow = document.createElement("tr")
      newRow.innerHTML=`
@@ -103,11 +124,11 @@ document.getElementById("expense-form").addEventListener("submit", function (e) 
      spend.innerText=expenditure
 
      //inserting the remaining amount to third div
-     const remaining = document.getElementById("remaining")
-     remaining.innerText=totalBudget==0?  0  :totalBudget-expenditure
+     remaining.innerText=totalBudgetFromLocalStorage==0? 0  :totalBudgetFromLocalStorage-expenditure
     }
   }
-
+  //created common Variable to store the index number
+  let indexToUpdate
   //writing logic to delete the data from the table
   tableData.addEventListener("click",(event)=>{
     // console.log(typeof parseInt(event.target.textContent))
@@ -118,26 +139,52 @@ document.getElementById("expense-form").addEventListener("submit", function (e) 
       localStorage.setItem("expenseData",JSON.stringify(expenseList))
     }
     
+    //WRITING LOGIC TO UPDATE THE DATA IN THE MODAL
     if(event.target.classList.contains("updateBtn"))
     {
-     let dataToUpdate= event.target.parentElement.parentElement
-     console.log(dataToUpdate)
-    }
+    const updateCateogory = expenseList[event.target.id].cateogory
+    const upadatePrice = expenseList[event.target.id].price
+    const updateDate = expenseList[event.target.id].date
+    indexToUpdate = event.target.id
+    console.log(+indexToUpdate)
+    console.log(updateCateogory,upadatePrice,updateDate)
+    // updateCateogoryinput.option.value=updateCateogoryinput
+    ////<this code is implimented if we want to update with SELECT instead of INPUT>
+    // updateCateogoryinput.value=updateCateogory
+    // updateOptioninput.textContent=updateCateogory
+    // console.log("value",updateOptioninput.value)
+    // console.log("textContent",updateOptioninput.textContent)
 
-
+    updateCateogoryinput.value=updateCateogory
+    updatePriceinput.value=upadatePrice
+    updateDateinput.value=updateDate
+  }
     renderList()
   })
   
-  
+  updateForm.addEventListener("submit",(event)=>{
+    event.preventDefault()
+    const finalOptionToUpdate=updateCateogoryinput.value
+    const finalPriceToUpdate=updatePriceinput.value
+    const finalDateToUpdate=updateDateinput.value
+    // console.log(finalOptionToUpdate,finalPriceToUpdate,finalDateToUpdate)
+    const updatedExpense =
+    {
+      cateogory:finalOptionToUpdate,
+      price:finalPriceToUpdate,
+      date:finalDateToUpdate
+    }
+    console.log(updatedExpense)
+    console.log(expenseList[indexToUpdate])
+    //CLEARING THE INPUT FIELDS AFTER THE SUBMISSION
+    updateCateogoryinput.value=""
+    updatePriceinput.value=""
+    updateDateinput.value=""
+
+  })
 
   //calling the function to show the data INITIALLY
   renderList()
-
-
-
-
-
-
 
 
   //rough data
