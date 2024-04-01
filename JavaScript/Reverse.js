a6ec3d53133497440bcb57d035e33a9589904316
VISHAL SINGH <daggercoding@gmail.com>
2024-04-01 23:57:03 +0530

    Reverse a Number

---
+++ JavaScript/Reverse.js
@@ -0,0 +1,28 @@
+let num = 276238479
+let ans = 0;
+
+while(num != 0){
+    let digit  = num % 10;
+    ans = (ans *10) + digit;
+    num= Math.floor(num/10)
+}
+console.log(ans)
+
+console.log(Math.floor(1.32))
+console.log()
+
+
+let num1 = 193278
+// console.log(num1.toString().split(""))
+let arr = []
+while(num1!==0){
+    let digit = num1 % 10;
+    arr.unshift(digit)
+    num1 = Math.floor(num1/ 10);
+
+}
+
+console.log(arr)
+
+
+
