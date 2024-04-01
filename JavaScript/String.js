00e29eafa9742887a0f8f9e951aaec95a3a0f742
VISHAL SINGH <daggercoding@gmail.com>
2024-04-01 23:58:23 +0530

    Find how many times a character is repreted in a string

--- JavaScript/String.js
+++ JavaScript/String.js
@@ -4,8 +4,18 @@ let converted=name.split(" ").map(el=>el.charAt(0).toUpperCase()+ el.substr(1))
 console.log(converted.join(" "))
 
 
-
-
+let name1 = "vishal singh is my name"
+let count = 0
+for(let val of name1){
+    if(val=="v"){
+        count++
+    }
+    
+}
+console.log(count)
+let name1 = "vishal"
+console.log(name1.substring(2,4))
+console.log(name1)
 
 
 
