149f08ab80710ad95f2747fe98c2aaba6768b817
VISHAL SINGH <daggercoding@gmail.com>
2024-04-03 23:51:48 +0530

    prime in js

--- JavaScript/Prime.js
+++ JavaScript/Prime.js
@@ -4,11 +4,12 @@ function isPrime(number){
     }
     for (let i=2;i<number;i++){
         if(number%i==0){
-            return `${number} is not a prime number`
+        return`${number} is not a prime number`
         }
     }
-   return `${number} is  a prime number`
+   console.log(`${number} is  a prime number`)
 }
+
 for(let i=0;i<=100;i++){
-    console.log(isPrime(i))
+    isPrime(i)
 }
