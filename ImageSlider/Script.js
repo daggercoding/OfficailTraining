46174f52d40b08f44d1af338a7813218d0e00438
VISHAL SINGH <daggercoding@gmail.com>
2024-04-08 23:51:35 +0530

    modified

--- ImageSlider/Script.js
+++ ImageSlider/Script.js
@@ -1,28 +1,33 @@
-let photos
-fetch("https://api.slingacademy.com/v1/sample-data/photos?offset=5&limit=132")
-.then((resp)=>resp.json())
-.then((data)=>{
-    photos=data.photos
-    render()
-})
-.catch((err)=>console.log(err.message))
+// let photos
+// fetch("https://api.slingacademy.com/v1/sample-data/photos?offset=5&limit=132")
+// .then((resp)=>resp.json())
+// .then((data)=>{
+//     photos=data.photos
+//     render()
+// })
+// .catch((err)=>console.log(err.message))
 
-function render()
-{
-let count = 0
-console.log(photos)
-setInterval(()=>{
-if(count<=photos.length-1)
-{
+// function render()
+// {
+// let count = 0
+// // console.log(photos)
+// setInterval(()=>{
+// if(count<=photos.length-1)
+// {
     
-    document.querySelector("h1").innerText=photos[count].title
-    container.innerHTML=`<img id="imgStyle" height="100%" width="100%" src="${photos[count].url}"/>`
-    count++
-}
-else{
-   count=0
-   console.log(`else ${count}`)
-}
-},300)   
+//     document.querySelector("h1").innerText=photos[count].title
+//     container.innerHTML=`<img id="imgStyle" height="100%" width="100%" src="${photos[count].url}"/>`
+//     count++
+//     console.log("working")
+// }
+// else{
+//    count=0
+//    console.log(`else ${count}`)
+// }
+// },300)   
 
-}
+// }
+setInterval(()=>{
+    console.log(" Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nulla atque aut. Provident a magni eius obcaecati eveniet repellendus numquam molestiae aut maxime voluptas perferendis atque cumque, possimus quod itaque, facilis aperiam. Corporis autem deserunt ut doloribus iusto dignissimos distinctio sequi nobis ullam delectus facilis numquam cumque doloremque maxime, qui quod repellendus molestiae libero culpa et modi fugiat adipisci minus? Fugit quaerat ipsam odio optio vel in excepturi mollitia distinctio iure officia est impedit numquam atque eaque non beatae possimus tempore, iste assumenda recusandae ullam alias. Accusantium repellat dolores atque perferendis, nesciunt blanditiis incidunt totam temporibus soluta ex necessitatibus, sit ipsum sed nostrum. Voluptatem laudantium ipsum, doloribus sapiente rerum tempore dolor sunt harum amet vitae possimus ab porro ea praesentium reprehenderit tenetur at sequi odit, vel ipsa illum? Commodi quaerat, ex modi ducimus sunt perferendis laudantium. Consectetur consequuntur odio impedit sed? Adipisci at nesciunt animi dolor quas repellat recusandae, dolorem itaque maxime provident dolores laudantium ab quibusdam. Tempora ad quo itaque veniam ducimus eligendi, doloremque nam illum provident odio ab similique ea, facere, modi laudantium molestias ipsam tempore nihil. Consequatur atque adipisci quo minus laborum, voluptatum iusto amet quaerat minima odit commodi quidem sapiente at, repudiandae corrupti libero veniam facere.")
+    console.log("testing")
+},1)
