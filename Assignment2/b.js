8ebddf52a5472b4473d55f451e8e531eecf62ffb
VISHAL SINGH <daggercoding@gmail.com>
2024-05-16 21:31:27 +0530

    deleted unnecessary file

--- Assignment2/b.js
+++
@@ -1,5 +0,0 @@
-function sayHello(){
- document.getElementById("greet").innerText="Hello Welcome to JavaScript tutorial."
-}
-
-sayHello()
--- Assignment2/c.js
+++
@@ -1,5 +0,0 @@
-function sayHello(string){
-    document.getElementById("greet").innerText=string
-   }
-   
-   sayHello("give any string i will print on the screen")
